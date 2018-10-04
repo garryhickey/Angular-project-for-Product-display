38d4e1036f26329174235c4a7944c47f916b26ee
Hickey <garry.hickey@sap.com>
2018-10-04 10:52:41 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+  selector: 'pm-root',
+  template:  `
+  <nav class='navbar navbar-expand navbar-light bg-light'>
+  <a class='navbar-brand'>{{pageTitle}}</a>
+  <ul class='nav nav-pills'>
+    <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+    <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
+  </ul>
+  </nav>
+  
+  <router-outlet></router-outlet>
+  
+
+`
+
+})
+
+export class AppComponent {
+  pageTitle :  string = 'Acme Product Management';
+}
